f72c9fd5c82deb510197930a55036301bea379db
sakshi <sakhsishar8054@gmail.com>
2025-05-04 19:55:07 +0530

    login and register Added

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const errorHandler=require('express-async-handler');
 const connectDB = require('./config/dbConnection');
+const auth=require("./routes/auth.js");
 
 const app = express();
 
@@ -14,6 +15,7 @@ connectDB();
 // middleware 
 app.use(express.json());
 app.use('/api/products',require('./routes/productRoutes'))
+app.use("/api",auth);//authentication
 app.use(errorHandler)
 
 app.listen(port , ()=>{
